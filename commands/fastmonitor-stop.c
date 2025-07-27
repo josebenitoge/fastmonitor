fcee25b41a70e3d085dca137dc4cd084b9b99005
josebenitoge <josebenitoedungomo@gmail.com>
2025-07-27 18:50:23 +0200

    Final stop

--- commands/fastmonitor-stop.c
+++ commands/fastmonitor-stop.c
@@ -10,15 +10,12 @@ int main() {
     FILE *fp;
     char line[MAX_LINE];
     pid_t pid;
-
     fp = popen("pgrep -f fastmonitor-start", "r");
     if (!fp) return 1;
-
     if (fgets(line, sizeof(line), fp)) {
         pid = (pid_t)atoi(line);
         if (pid > 0) kill(pid, SIGTERM);
     }
-
     pclose(fp);
     return 0;
 }
