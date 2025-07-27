a52e5732ff50f6c16120c6e053bf3c3c4539a27c
josebenitoge <josebenitoedungomo@gmail.com>
2025-07-27 18:34:55 +0200

    Stop

--- commands/fastmonitor-stop.c
+++ commands/fastmonitor-stop.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <signal.h>
 #include <unistd.h>
 
+#define MAX_LINE 256
+
 int main() {
-    FILE *fp = fopen("fastmonitor.pid", "r");
-    if (!fp) {
-	//perror("Stop error: Unexpected state");
-        return 1;
+    FILE *fp;
+    char line[MAX_LINE];
+    pid_t pid;
+
+    fp = popen("pgrep -f fastmonitor-start", "r");
+    if (!fp) return 1;
+
+    if (fgets(line, sizeof(line), fp)) {
+        pid = (pid_t)atoi(line);
+        if (pid > 0) kill(pid, SIGTERM);
     }
-    int pid;
-    if (fscanf(fp, "%d", &pid) != 1) {
-        fclose(fp);
-	perror("Stop error: Corrupted state");
-        return 1;
-    }
-    fclose(fp);
-    if (kill(pid, SIGTERM) == 0) {
-        printf("FastMonitor (%d) stopped.\n", pid);
-        remove("fastmonitor.pid");
-    } else {
-        perror("Error. Couldn't stop Fast Monitor");
-    }
+
+    pclose(fp);
     return 0;
 }
-
