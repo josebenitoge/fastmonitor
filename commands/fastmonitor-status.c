fcd527c8736eb830b2142a977bdbdc3f58c6b6a7
josebenitoge <josebenitoedungomo@gmail.com>
2025-07-27 18:28:37 +0200

    Cambios en status

--- commands/fastmonitor-status.c
+++ commands/fastmonitor-status.c
@@ -7,52 +7,66 @@
 #define COLOR_RESET "\033[0m"
 #define MAX_LINE    512
 
-void check_monitor_process() {
-    FILE *pid_file = fopen("fastmonitor.pid", "r");
-    char pid[16] = {0};
-
-    if (!pid_file) {
-        printf("Status: Off\n");
-        return;
-    }
-
-    if (!fgets(pid, sizeof(pid), pid_file)) {
-        printf("Status: Off. Process corrupted\n");
-        fclose(pid_file);
-        return;
-    }
-
-    pid[strcspn(pid, "\n")] = 0;
-    fclose(pid_file);
-
-    char cmd[MAX_LINE];
-    snprintf(cmd, sizeof(cmd), "ps -p %s > /dev/null", pid);
-
-    if (system(cmd) == 0) {
-        printf("Status: " COLOR_GREEN "Running" COLOR_RESET " (PID: %s)\n", pid);
-    } else {
-        printf("Status: Off. Unexpected state\n");
-    }
-}
-
-void run_monitor_conf() {
+int check_monitor_process(char *out_pid, size_t pid_size, int verbose) {
     FILE *fp;
     char line[MAX_LINE];
 
-    fp = popen("./fastmonitor-conf", "r");
+    fp = popen("pgrep -f fastmonitor-start", "r");
     if (!fp) {
-        return;
+        if (verbose) printf("Status: Error checking process\n");
+        return -1;
     }
 
-    while (fgets(line, sizeof(line), fp)) {
-        printf("%s", line); // monitor-conf ya incluye color
+    if (fgets(line, sizeof(line), fp)) {
+        line[strcspn(line, "\n")] = 0;
+        strncpy(out_pid, line, pid_size - 1);
+        pclose(fp);
+        if (verbose) {
+            printf("Status: " COLOR_GREEN "Running" COLOR_RESET " (PID: %s)\n", out_pid);
+        }
+        return 1;
     }
 
     pclose(fp);
+    if (verbose) {
+        printf("Status: Not running\n");
+    }
+    return 0;
 }
 
-int main() {
-    check_monitor_process();
-    run_monitor_conf();
-    return 0;
+int run_monitor_conf(int verbose) {
+    FILE *fp;
+    char line[MAX_LINE];
+    int result = 0;
+
+    fp = popen("./fastmonitor-conf verbose", "r");
+    if (!fp) {
+        if (verbose) fprintf(stderr, "Error: Unable to run fastmonitor-conf\n");
+        return 2;
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+        if (verbose) printf("%s", line);
+    }
+
+    int exit_code = pclose(fp);
+    if (WIFEXITED(exit_code)) {
+        result = WEXITSTATUS(exit_code);
+    } else {
+        result = 2;
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    int verbose = argc > 1;
+    char pid[16] = {0};
+
+    int monitor_status = check_monitor_process(pid, sizeof(pid), verbose);
+    int conf_status = run_monitor_conf(verbose);
+
+    if (conf_status != 0) return 2;
+    if (monitor_status == 1) return 0;
+    return 1;
 }
